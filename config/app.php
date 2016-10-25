a4e7a709813b0889c660aabff09f288b25b7960f
Katie Kujala <katie_kujala@harvard.edu>
2016-10-25 17:42:40 -0400

    removed package I did not use

--- config/app.php
+++ config/app.php
@@ -177,7 +177,6 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        Magyarjeti\LaravelLipsum\LipsumServiceProvider::class,
 
     ],
 
@@ -226,7 +225,6 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'Lipsum' => Magyarjeti\LaravelLipsum\LipsumFacade::class,
         'Debugbar' => Barryvdh\Debugbar\Facade::class
 
     ],
