0cdbb43a30d4b5e4457aa3d5e1f37af00a32e3e9
Katie Kujala <katie_kujala@harvard.edu>
2016-10-25 23:26:51 -0400

    li store page do it placement fixed

--- resources/views/lorem-ipsum/store.blade.php
+++ resources/views/lorem-ipsum/store.blade.php
@@ -9,10 +9,12 @@ Lorem Ipsum Generation
     <a href='/'>&larr; Home</a>
 </div>
 <div class="row">
-    <div class="col-md-12">
+    <div class="col-md-8">
         <h3>Lorem Ipsum Generation</h3>
     </div>
+    <div class="col-md-4">
     <a href="/lorem-ipsum/create">Do it again!</a>
+</div>
 </div>
 <div class="row">
     <div class="col-md-12" style="background-color:#BFE5D8;">
